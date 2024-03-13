cec13006f6b48c4036cdf8f447661370a5c0d992
Aramy Ferrent <129014074+AramyJFerrent@users.noreply.github.com>
2024-03-13 11:30:51 -0400

    Update Assessment.h

--- Gradebook/Assessment.h
+++ Gradebook/Assessment.h
@@ -1,3 +1,8 @@
+/* 
+ * This file contains the declaration of the Assessment class, which represents an assessment (e.g., exam, quiz, project) in a student grading system. 
+ * It includes a pure virtual method to get the grade of the assessment, as well as methods to get and set the assessment's name.
+ */
+
 #ifndef ASSESSMENT_H
 #define ASSESSMENT_H
 
@@ -5,12 +10,17 @@
 
 class Assessment {
 public:
+    // Constructor
     Assessment(const std::string& name);
+    // Virtual destructor
     virtual ~Assessment() = default;
 
+    // Pure virtual method to get the grade
     virtual double getGrade() const = 0;
 
+    // Getter for name
     const std::string& getName() const;
+    // Setter for name
     void setName(const std::string& name);
 
 private:
