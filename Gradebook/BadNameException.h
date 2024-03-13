b63ea3d4aacc958fbda370d10829806148f25b87
Aramy Ferrent <129014074+AramyJFerrent@users.noreply.github.com>
2024-03-13 11:34:07 -0400

    Update BadNameException.h

--- Gradebook/BadNameException.h
+++ Gradebook/BadNameException.h
@@ -1,3 +1,8 @@
+/* 
+ * This file contains the declaration of the BadNameException class, which is used to represent exceptions related to bad names (e.g., empty names) in the program. 
+ * It inherits from std::exception and includes a constructor that takes a message as input, as well as a method to retrieve the exception message.
+ */
+
 #ifndef BADNAMEEXCEPTION_H
 #define BADNAMEEXCEPTION_H
 
@@ -6,7 +11,9 @@
 
 class BadNameException : public std::exception {
 public:
+    // Constructor
     BadNameException(const std::string& message);
+    // Method to retrieve the exception message
     const char* what() const noexcept override;
 private:
     std::string message;
