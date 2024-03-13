65555c7ba9381d2a4da98d4ae9f35eb04cc6717a
Aramy Ferrent <129014074+AramyJFerrent@users.noreply.github.com>
2024-03-13 11:44:00 -0400

    Update UnknownStudentException.h

--- Gradebook/UnknownStudentException.h
+++ Gradebook/UnknownStudentException.h
@@ -1,3 +1,8 @@
+/* 
+ * This file contains the declaration of the UnknownStudentException class, which is used to represent exceptions related to unknown students in the program. 
+ * It inherits from std::exception and includes a constructor that takes a student name as input, as well as a method to retrieve the exception message.
+ */
+
 #ifndef UNKNOWNSTUDENTEXCEPTION_H
 #define UNKNOWNSTUDENTEXCEPTION_H
 
@@ -6,7 +11,9 @@
 
 class UnknownStudentException : public std::exception {
 public:
+    // Constructor
     UnknownStudentException(const std::string& studentName);
+    // Method to retrieve the exception message
     const char* what() const noexcept override;
 private:
     std::string studentName;
