f6d415a15da8e8badfc68dd249f5aa7eaa43d0f1
Aramy Ferrent <129014074+AramyJFerrent@users.noreply.github.com>
2024-03-13 11:39:08 -0400

    Update Essay.h

--- Gradebook/Essay.h
+++ Gradebook/Essay.h
@@ -1,3 +1,8 @@
+/* 
+ * This file contains the declaration of the Essay class, which represents an essay assessment in a student grading system. 
+ * It inherits from the Assessment class and includes methods to get and set the essay's grade.
+ */
+
 #ifndef ESSAY_H
 #define ESSAY_H
 
@@ -5,8 +10,11 @@
 
 class Essay : public Assessment {
 public:
+    // Constructor
     Essay(const std::string& name);
+    // Method to get the essay's grade
     double getGrade() const;
+    // Method to set the essay's grade, with bounds checking
     void setGrade(double newGrade);
 
 private:
